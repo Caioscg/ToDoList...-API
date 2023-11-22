70878011adc520287513bec9bf5d88ed701704e5
Caioscg <cacazaok5@gmail.com>
2023-11-21 23:40:49 -0300

    google-api

--- src/server.js
+++ src/server.js
@@ -10,5 +10,23 @@ const auth2Client = new google.auth.OAuth2(
     process.env.REDIRECT_URL
 )
 
+const scopes = [
+    'https://www.googleapis.com/auth/calendar'
+]
+
+app.get("/google", (req, res) => {
+
+    const url = auth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: scopes
+    })
+
+    res.redirect(url)
+})
+
+app.get("/google/redirect", (req, res) => {
+    res.send("It's working!")
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
